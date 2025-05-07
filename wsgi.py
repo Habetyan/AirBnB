36893994c305fbc9551ddc60a0268d4818ea3280
Habetyan <90478713+Habetyan@users.noreply.github.com>
2025-05-07 22:56:32 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import app.pages.home
 import app.pages.geo_visualizations
 import app.pages.technical
 
-server = app.server  # This is important for gunicorn to find the server
+server = app.server 
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=10000)
