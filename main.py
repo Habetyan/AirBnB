38f68bef78535237b6e0bb5b6948e1f5138da8d9
Habetyan <90478713+Habetyan@users.noreply.github.com>
2025-05-07 23:57:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from app.app import app
+#from app.app import app
 
-if __name__ == "__main__":
-    print("Starting Texas Airbnb Dashboard...")
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    print("Starting Texas Airbnb Dashboard...")
+#    app.run(debug=True)
