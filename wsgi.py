651edb345f05ed72d969feb4909dc0d40320e13c
Habetyan <90478713+Habetyan@users.noreply.github.com>
2025-05-07 23:12:56 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,10 @@
-import os
-from app.app import app
+from app.app import app as flask_app  # Renamed to avoid namespace conflict
 import app.pages.home
 import app.pages.geo_visualizations
 import app.pages.technical
 
-server = app.server
+server = flask_app.server  # Use the renamed variable
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    flask_app.run(debug=False, host='0.0.0.0', port=port)
