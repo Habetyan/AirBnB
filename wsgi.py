5988e37953996aae4130cd031a7e95e705eded7a
Habetyan <90478713+Habetyan@users.noreply.github.com>
2025-05-07 23:05:44 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
-# app.py in the root directory
+import os
 from app.app import app
 import app.pages.home
 import app.pages.geo_visualizations
 import app.pages.technical
 
-server = app.server 
+server = app.server
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
